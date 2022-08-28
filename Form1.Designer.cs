5a1d7bbb5d5ace7e84eec35c50f89839bac49742
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-08-28 15:50:20 +0545

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageTeacherToolStripMenuItem,
+            this.manageStudentToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // manageTeacherToolStripMenuItem
+            // 
+            this.manageTeacherToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addTeacherToolStripMenuItem,
+            this.displayTeacherToolStripMenuItem});
+            this.manageTeacherToolStripMenuItem.Name = "manageTeacherToolStripMenuItem";
+            this.manageTeacherToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.manageTeacherToolStripMenuItem.Text = "manage teacher";
+            this.manageTeacherToolStripMenuItem.Click += new System.EventHandler(this.manageTeacherToolStripMenuItem_Click);
+            // 
+            // manageStudentToolStripMenuItem
+            // 
+            this.manageStudentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addStudentToolStripMenuItem,
+            this.displayStudentToolStripMenuItem});
+            this.manageStudentToolStripMenuItem.Name = "manageStudentToolStripMenuItem";
+            this.manageStudentToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.manageStudentToolStripMenuItem.Text = "manage student";
+            // 
+            // addStudentToolStripMenuItem
+            // 
+            this.addStudentToolStripMenuItem.Name = "addStudentToolStripMenuItem";
+            this.addStudentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addStudentToolStripMenuItem.Text = "Add Student";
+            this.addStudentToolStripMenuItem.Click += new System.EventHandler(this.addStudentToolStripMenuItem_Click);
+            // 
+            // displayStudentToolStripMenuItem
+            // 
+            this.displayStudentToolStripMenuItem.Name = "displayStudentToolStripMenuItem";
+            this.displayStudentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.displayStudentToolStripMenuItem.Text = "display student";
+            // 
+            // addTeacherToolStripMenuItem
+            // 
+            this.addTeacherToolStripMenuItem.Name = "addTeacherToolStripMenuItem";
+            this.addTeacherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addTeacherToolStripMenuItem.Text = "add teacher";
+            // 
+            // displayTeacherToolStripMenuItem
+            // 
+            this.displayTeacherToolStripMenuItem.Name = "displayTeacherToolStripMenuItem";
+            this.displayTeacherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.displayTeacherToolStripMenuItem.Text = "display teacher";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem manageTeacherToolStripMenuItem;
+        private ToolStripMenuItem addTeacherToolStripMenuItem;
+        private ToolStripMenuItem displayTeacherToolStripMenuItem;
+        private ToolStripMenuItem manageStudentToolStripMenuItem;
+        private ToolStripMenuItem addStudentToolStripMenuItem;
+        private ToolStripMenuItem displayStudentToolStripMenuItem;
+    }
+}
