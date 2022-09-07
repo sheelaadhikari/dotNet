cbec6f78d8aa2a6ebeb5daccb76728dba3f3e760
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-09-07 20:00:27 +0545

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Abstract
+{
+    internal class Rectangle:Shape
+    {
+        int l, b;
+    public Rectangle(int length, int breadth)
+        {
+            l = length;
+            b = breadth;
+        }
+        public override int Area()
+        {
+            return l*b;   
+        }
+
+    }
+}
