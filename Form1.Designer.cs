ddab5fc7a1cab35224b8b5da4f587e92c2bb02e9
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-09-07 20:02:18 +0545

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace WinFormsApp1
+﻿namespace Lab_Abstract
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,100 +23,39 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.manageTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.manageStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.displayStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.displayTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // button2
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.manageTeacherToolStripMenuItem,
-            this.manageStudentToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // manageTeacherToolStripMenuItem
-            // 
-            this.manageTeacherToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addTeacherToolStripMenuItem,
-            this.displayTeacherToolStripMenuItem});
-            this.manageTeacherToolStripMenuItem.Name = "manageTeacherToolStripMenuItem";
-            this.manageTeacherToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
-            this.manageTeacherToolStripMenuItem.Text = "manage teacher";
-            this.manageTeacherToolStripMenuItem.Click += new System.EventHandler(this.manageTeacherToolStripMenuItem_Click);
-            // 
-            // manageStudentToolStripMenuItem
-            // 
-            this.manageStudentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addStudentToolStripMenuItem,
-            this.displayStudentToolStripMenuItem});
-            this.manageStudentToolStripMenuItem.Name = "manageStudentToolStripMenuItem";
-            this.manageStudentToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
-            this.manageStudentToolStripMenuItem.Text = "manage student";
-            // 
-            // addStudentToolStripMenuItem
-            // 
-            this.addStudentToolStripMenuItem.Name = "addStudentToolStripMenuItem";
-            this.addStudentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.addStudentToolStripMenuItem.Text = "Add Student";
-            this.addStudentToolStripMenuItem.Click += new System.EventHandler(this.addStudentToolStripMenuItem_Click);
-            // 
-            // displayStudentToolStripMenuItem
-            // 
-            this.displayStudentToolStripMenuItem.Name = "displayStudentToolStripMenuItem";
-            this.displayStudentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.displayStudentToolStripMenuItem.Text = "display student";
-            // 
-            // addTeacherToolStripMenuItem
-            // 
-            this.addTeacherToolStripMenuItem.Name = "addTeacherToolStripMenuItem";
-            this.addTeacherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.addTeacherToolStripMenuItem.Text = "add teacher";
-            // 
-            // displayTeacherToolStripMenuItem
-            // 
-            this.displayTeacherToolStripMenuItem.Name = "displayTeacherToolStripMenuItem";
-            this.displayTeacherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.displayTeacherToolStripMenuItem.Text = "display teacher";
+            this.button2.Location = new System.Drawing.Point(350, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Display Area";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem manageTeacherToolStripMenuItem;
-        private ToolStripMenuItem addTeacherToolStripMenuItem;
-        private ToolStripMenuItem displayTeacherToolStripMenuItem;
-        private ToolStripMenuItem manageStudentToolStripMenuItem;
-        private ToolStripMenuItem addStudentToolStripMenuItem;
-        private ToolStripMenuItem displayStudentToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
     }
 }
+
