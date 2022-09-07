10e950ed70b5c379b272e54fe37809121802c9b6
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-09-07 20:00:06 +0545

    Add files via upload

---
+++ ShapeProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Abstract
+{
+  public class ShapeProvider
+    {
+public Shape GetShape(string shapeType)
+        {
+            switch (shapeType) {
+                case "circle":
+                    return new Circle(4);
+                case "rectangle":
+                    return new Rectangle(3, 4);
+
+                 default:
+                    return new Circle(4);
+                    break;     
+            
+            }
+           
+        }
+
+    }
+}
