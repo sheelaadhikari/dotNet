3f22153d17dd6ebecf92d7f1f3257d7aacf0726b
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-09-08 21:37:16 +0545

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Abstract
+{
+ public class Circle: Shape
+    {
+        int r;
+        public Circle (int radius)
+        {
+            r = radius;
+        }
+        public override int Area()
+        {
+            return 3 * r * r;
+        }
+    }
+}
