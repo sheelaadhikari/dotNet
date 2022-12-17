081c5f91ec051f0dbdec6dcac7062ce456a414a5
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-12-17 20:00:45 +0545

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShopping.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public virtual IList<Product>?Products{ get; set; }
+        
+
+
+    }
+}
