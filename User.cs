71e5f76b0cd9817f9427e50f34a323321c3cc50b
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-12-17 20:01:36 +0545

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage ="Email should be less than 50 characters")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Password should be less than 50 characters")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+
+
+    }
+}
