d0ad1342a8a26e9a68340af781ccf692e6894987
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-12-19 16:59:32 +0545

    Add files via upload

---
+++ UserController.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+
+namespace OnlineShopping.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly OnlineShoppingContext _context;
+
+        public UserController(OnlineShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkUser = (from u in await _context.User.ToListAsync()
+                                 where u.Email == user.Email
+                                 select u).ToList();
+                if (checkUser.Count > 0)
+                {
+                    ViewData["ErrorMessage"] = "User alreadsy exist";
+                }
+                else
+                {
+                    _context.Add(user);
+                    await _context.SaveChangesAsync();
+                    ViewData["ErrorMessage"] = "User successfully registered";
+                }
+
+
+                // return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkUser = (from u in await _context.User.ToListAsync()
+                                 where u.Email == user.Email
+                                 select u).ToList();
+                if (checkUser.Count > 0)
+                {
+                    ViewData["ErrorMessage"] = "User or password you entered doesnot exist";
+                }
+                else
+                {
+                    var claims = new List<Claim>();
+                    var Claim = new Claim()(ClamTypes.Email, user.Email);
+                    claims.Add(Claim);
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    await HttpContext.SignInAsync( CookieAuthenticationDefaults.AuthenticationScheme,new ClaimsPrincipal(identity));
+                    return RedirectToAction("Index", "Home");
+                            
+
+
+                   // _context.Add(user);
+                   // await _context.SaveChangesAsync();
+                   // ViewData["ErrorMessage"] = "User successfully registered";
+                }
+
+
+                // return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.User.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Password")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.User == null)
+            {
+                return Problem("Entity set 'OnlineShoppingContext.User'  is null.");
+            }
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
